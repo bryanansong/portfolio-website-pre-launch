087723521f158ece1c3725e4938b94ef447ee0e6
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-01-07 22:15:05 -0500

    Initialized Vite and added background colors

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+	return (
+		<main className="h-svh w-svw ">
+			<div
+				className="w-full
+        h-screen
+        bg-gradient-to-r
+        from-pink-200
+        via-orange-100
+        to-yellow-200
+        background-animate"
+			>
+				<div className=" ">Welcome to Bryan's Portfolio</div>
+				<div>Website Coming Soon!</div>
+			</div>
+		</main>
+	);
+}
