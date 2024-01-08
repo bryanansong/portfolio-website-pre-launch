924bce244a67ab79f65ad8feb413529a7ee34360
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-01-07 23:13:00 -0500

    Added Text and glowing background

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,30 @@ export default function App() {
 			<div
 				className="w-full
         h-screen
-        bg-gradient-to-r
-        from-pink-200
-        via-orange-100
-        to-yellow-200
-        background-animate"
+        bg-yellow-100
+        background-animate
+        flex flex-col
+        items-center
+        justify-center"
 			>
-				<div className=" ">Welcome to Bryan's Portfolio</div>
-				<div>Website Coming Soon!</div>
+				<div className=" font-montserrat text-2xl mb-12">
+					Welcome to Bryan's Portfolio!
+				</div>
+				<div className="relative">
+					<div
+						className="absolute inset-0 bg-gradient-to-r from-pink-600
+            to-orange-600 rounded-2xl blur-xl opacity-55"
+					></div>
+					<div
+						className=" relative ring-1 ring-gray-700/5 rounded-2xl
+            px-6 py-2 shadow-md bg-yellow-200"
+					>
+						<div className=" font-montserrat text-4xl m-6">
+							Website Coming Soon
+							<span className="loading"></span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</main>
 	);
